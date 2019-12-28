3289c1b2769aa99854a50fdfc6a0b6100e43fb88
manxihuanchi <546568501@qq.com>
2019-12-28 10:43:01 +0800

    11

--- src/test/java/cho/carbon/SpringbootCarbonProviderApplicationTests.java
+++ src/test/java/cho/carbon/SpringbootCarbonProviderApplicationTests.java
@@ -1,11 +1,32 @@
 package cho.carbon;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootCarbonProviderApplicationTests {
 
+	@Autowired
+	AmqpAdmin amqpAdmin;
+	
+	@Test
+	public void createExchange() {
+		String exchange = "carbon.fg";
+		amqpAdmin.declareExchange(new DirectExchange(exchange));
+		
+		String queue = "carbonqueu";
+		amqpAdmin.declareQueue(new Queue(queue, true));
+		
+		String routingKey = "carbonqueukey";
+		amqpAdmin.declareBinding(new Binding(queue, Binding.DestinationType.QUEUE, exchange, routingKey, null));
+	}
+	
+	
 	@Test
 	void contextLoads() {
 	}
